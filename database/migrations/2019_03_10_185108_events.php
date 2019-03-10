bd12179c920a4f04ce0d9a9cabf6d62c682df273
Jimenoza <luispama96@hotmail.com>
2019-03-10 12:54:17 -0600

    Migraciones para eventos y tipos de eventos

---
+++ database/migrations/2019_03_10_185108_events.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Events extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date('eventDate');
+            $table->dateTime('start');
+            $table->dateTime('finish');
+            $table->string("image")->nullable();
+            $table->string("name");
+            $table->unsignedInteger('eventType');
+            $table->foreign('eventType')->references('id')->on('eventTypes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
