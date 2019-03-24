31b833c7bf3c9574d9088ecf1a8b56147ade9bfa
JoVarHdez <papinchovargas@gmail.com>
2019-03-24 16:49:17 -0600

    Imagen Opcional Faker
    Se agrega que la imagen sea opcional en el Factory

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -8,7 +8,7 @@ $factory->define(App\Models\Event::class, function (Faker $faker) {
         'eventDate'=> $faker->date($format = 'Y-m-d', $max = 'now'),
         'start'=> $faker->time($format = 'H:i:s', $max = 'now'),
         'finish'=> $faker->time($format = 'H:i:s', $max = 'now'),
-        'image'=> $faker->imageUrl($width = 640, $height = 480),
+        'image'=> $faker->optional()->imageUrl($width = 640, $height = 480),
         'name'=> $faker->word
     ];
 });
