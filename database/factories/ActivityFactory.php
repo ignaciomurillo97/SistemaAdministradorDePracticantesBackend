5c6de0df88f7d9f2179b00490f37e69fc49d5625
JoVarHdez <papinchovargas@gmail.com>
2019-03-26 13:14:26 -0600

    Update ActivityFactory.php
    Se corrigen los atributos del Activity Factory

--- database/factories/ActivityFactory.php
+++ database/factories/ActivityFactory.php
@@ -5,7 +5,11 @@ use Faker\Generator as Faker;
 $factory->define(App\Models\Activity::class, function (Faker $faker) {
     return [
     	'id'=> $faker->unique()->randomNumber(),
-    	'start'=> $faker->time($format = 'H:i:s', $max = 'now'),
-    	'finish'=> $faker->time($format = 'H:i:s', $max = 'now'),
+    	'duration'=> $faker->time($format = 'H:i:s', $max = 'now'),
+    	'activityName'=> $faker->sentence($nbWords = 4, $variableNbWords = true),
+    	'charlista'=>$faker->name,
+    	'remarks'=>$faker->optional()->text($maxNbChars = 200)
     ];
 });
+
+// 'duration','event_id','company_id','activityName','charlista','remarks']
