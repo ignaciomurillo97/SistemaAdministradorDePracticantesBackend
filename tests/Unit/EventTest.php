f61d2beedcc5e07cd7a2fd15b03954e819adc038
JoVarHdez <papinchovargas@gmail.com>
2019-03-26 13:13:11 -0600

    Testeo de Eventos con Usuario
    Se agrega el usuario a los test

--- tests/Unit/EventTest.php
+++ tests/Unit/EventTest.php
@@ -8,6 +8,8 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use App\Models\EventType;
 use App\Models\Event;
+use App\Models\User;
+use Laravel\Passport\Passport;
 
 class EventTest extends TestCase
 {
@@ -19,6 +21,11 @@ class EventTest extends TestCase
      */
     public function test_GetAllEvents_RequestApi_ReturnsAllEvents()
     {
+        Passport::actingAs(
+            User::where('scope_id', '1')->first(),
+            ['super-user']
+        );
+
     	$eventType = factory(EventType::class)->create();
     	$event = factory(Event::class, 10)->create([
     		'type_id' => $eventType->id
@@ -31,6 +38,11 @@ class EventTest extends TestCase
     }
 
     public function test_createEvent_POSTRequest_AppearsInDatabase() {
+        Passport::actingAs(
+            User::where('scope_id', '1')->first(),
+            ['super-user']
+        );
+        
     	// Add Event Type
     	$this->json('POST', "/api/eventTypes", ['name' => 'eventType']);
     	$result = $this->json('GET', '/api/eventTypes');
@@ -41,7 +53,7 @@ class EventTest extends TestCase
         	'date'=>'2019-03-12', 
         	'start'=>'10:10:00',
         	'finish'=>'11:10:02',
-        	'image'=>'http://lorempixel.com/640/480/',
+        	'image'=>'C:\Users\example\Desktop\image.png',
         	'type'=>$type_id]);
         $this->assertDatabaseHas('events', ['name' => 'event']);
     }
