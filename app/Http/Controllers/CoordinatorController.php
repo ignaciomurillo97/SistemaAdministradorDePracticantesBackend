473182a40fbb2d69a268bfd610416187184cd4db
JoVarHdez <papinchovargas@gmail.com>
2019-06-06 14:18:58 -0600

    Update CoordinatorController.php

--- app/Http/Controllers/CoordinatorController.php
+++ app/Http/Controllers/CoordinatorController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Person;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Input;
 
 class CoordinatorController extends Controller
 {
