8202c2aadbe1b0702f3835a3e21e9cb0446703a9
Ignacio Murillo <ignaciomurillo97@gmail.com>
2019-06-09 14:32:42 +0000

    passport middleware en las rutas de evaluacion para que solo el estudiante pueda emitir una evaluacion

--- routes/api.php
+++ routes/api.php
@@ -81,8 +81,12 @@ Route::post('/student/{id}/reject', "StudentController@rejectStudent")
 Route::post('/students/assign/{student}/{professor}','StudentController@assignProfessor')
     ->middleware('auth:api')
     ->middleware('scope:super-user,coordinator');
-Route::post('student/evaluation/internship-process', 'StudentController@storeInternshipProcessEvaluation');
-Route::post('student/evaluation/internship-professor', 'StudentController@storeInternshipProfessorEvaluation');
+Route::post('student/evaluation/internship-process', 'StudentController@storeInternshipProcessEvaluation')
+    ->middleware('auth:api')
+    ->middleware('scope:student');
+Route::post('student/evaluation/internship-professor', 'StudentController@storeInternshipProfessorEvaluation')
+    ->middleware('auth:api')
+    ->middleware('scope:student');
 
 // Person
 Route::get('/person/{id}', "PersonController@index");
