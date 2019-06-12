f5913b8e52802073e207838a5ec4264e57c5b0a3
Jimenoza <luispama96@hotmail.com>
2019-06-11 22:57:57 -0600

    Cambios menores

--- database/factories/StudentFactory.php
+++ database/factories/StudentFactory.php
@@ -8,7 +8,7 @@ $factory->define(App\Models\Student::class, function (Faker $faker) {
         'person_id' => 0,
         'career_id' => getInstanceFromDB(App\Models\Career::class)->id,
         'site_id' => getInstanceFromDB(App\Models\Site::class)->id,
-        'status' => $faker->randomElement(['Aprobado','Rechazado','Pendiente']),
+        'status' => $faker->randomElement([1,2,3]),
         'semester_id' => $faker->randomElement([1, 2]),
         'image' => '/image/tmp.jpg'
     ];
